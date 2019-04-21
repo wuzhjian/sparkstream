957586aea1d47661fea35232edf8e7a03ca263f2
wu_zhjian <446440221@qq.com>
2019-04-21 22:44:52 +0800

    实时统计

---
+++ src/main/scala/stream/app/CustomDirectKafkaExample.scala
@@ -0,0 +1,155 @@
+package stream.app
+
+
+import javax.servlet.http.{HttpServletRequest, HttpServletResponse}
+import kafka.common.TopicAndPartition
+import kafka.message.MessageAndMetadata
+import kafka.serializer.StringDecoder
+import kafka.utils.ZKStringSerializer
+import org.I0Itec.zkclient.ZkClient
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.fs.Path
+import org.apache.kafka.clients.consumer.ConsumerRecord
+import org.apache.kafka.common.TopicPartition
+import org.apache.spark.SparkConf
+import org.apache.spark.streaming.dstream.InputDStream
+import org.apache.spark.streaming.kafka010.KafkaUtils
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+import org.slf4j.{Logger, LoggerFactory}
+import org.spark_project.jetty.server.{Request, Server}
+import org.spark_project.jetty.server.handler.{AbstractHandler, ContextHandler}
+import utils.{KafkaOffsetManager, PropertiesUtil}
+import org.apache.spark.streaming.kafka010.LocationStrategies.PreferConsistent
+import org.apache.spark.streaming.kafka010.ConsumerStrategies.Subscribe
+
+import scala.tools.nsc.interpreter.InputStream
+
+object CustomDirectKafkaExample {
+  val logger = LoggerFactory.getLogger(CustomDirectKafkaExample.getClass)
+
+
+
+
+  def main(args: Array[String]): Unit = {
+    val conf = new SparkConf().setMaster("local[2]")
+      .setAppName("CustomDirectKafkaExample")
+    val ssc = new StreamingContext(conf, Seconds(5))
+
+    val topics = PropertiesUtil.getPropString("kafka.topics").split(",").toSet
+    val kafkaParams = Map[String, String] (
+      "metadata.broker.list" -> PropertiesUtil.getPropString("metadata.broker.list"),
+      "auto.offset.reset" -> PropertiesUtil.getPropString("auto.offset.reset"),
+      "group.id" -> PropertiesUtil.getPropString("group.id"),
+      "serializer.class" -> "kafka.serializer.StringEncoder"
+    )
+
+    val zkClient = new ZkClient(PropertiesUtil.getPropString("zkclients"), 3000, 3000)
+    val zkOffsetPath = "/sparkstreaming/20171128"
+    val message:InputDStream[ConsumerRecord[String, String]] = creaetDirectStream(ssc, kafkaParams, zkClient, zkOffsetPath, topics)
+
+    message.foreachRDD(rdd =>{
+      if (!rdd.isEmpty()){
+        rdd.foreachPartition(partition=>{
+          //如果没有使用广播变量，连接资源就在这个地方初始化
+          //比如数据库连接，hbase，elasticsearch，solr，等等
+
+          partition.foreach(msg=>{
+            logger.info("读取的数据: " + msg)
+            // process(msg)  处理每条数据
+          })
+        })
+        // 更新每隔批次的偏移量到zk中，这段代码允许在driver上
+        KafkaOffsetManager.saveOffsets(zkClient, zkOffsetPath, rdd)
+      }
+    })
+    ssc.start()
+
+    // 启动接收停止请求的守护进程
+    daemonHttpServer(5555, ssc)
+    
+
+  }
+
+
+  def creaetDirectStream(ssc: StreamingContext,
+                         kafkaParams: Map[String, String],
+                         zkClient: ZkClient,
+                         zkOffsetPath: String,
+                         topics: Set[String]): InputDStream[ConsumerRecord[String, String]] = {
+    // 目前仅支持一个topic的偏移量处理，读取zk里面偏移量字符串
+    val zkOffsetDate = KafkaOffsetManager.readOffsets(zkClient, zkOffsetPath, topics.last)
+    val kafkaStream = zkOffsetDate match {
+      case None =>
+        logger.info("系统第一次启动，没有读取到偏移量，默认从最新的offset开始消费")
+        KafkaUtils.createDirectStream[String, String](ssc, PreferConsistent, Subscribe[String, String](topics, kafkaParams))
+      case Some(lastStopOffset) =>
+        logger.info("从zk中读取到偏移量，从上次的偏移量开始消费数据........")
+        val topicPartition = new TopicPartition(topics.last, lastStopOffset.last._1.partition)
+        val ss = Map[TopicPartition, Long](
+          topicPartition -> lastStopOffset.last._2
+        )
+
+        // val messageHandler = (mmd: MessageAndMetadata[String, String]) =>(mmd.key, mmd.message())
+        //使用上次停止时候的偏移量创建DirectStream
+
+        KafkaUtils.createDirectStream(ssc, PreferConsistent, Subscribe[String, String](topics, kafkaParams, ss))
+    }
+    kafkaStream
+  }
+
+  /**
+    * 判断是否存在mark file
+    * @param hdfs_file_path
+    * @return
+    */
+  def isExistsMarkFile(hdfs_file_path: String): Boolean = {
+    val conf = new Configuration()
+    val path = new Path(hdfs_file_path)
+    val fs = path.getFileSystem(conf)
+    fs.exists(path)
+  }
+
+  def stopByMarkFile(ssc: StreamingContext)={
+    val intervalMills = 10 * 1000 //每隔10秒扫描一次消息是否存在
+    var isStop = false
+    val hdfs_file_path = "/spark/streaming/stop"
+    while (!isStop){
+      isStop = ssc.awaitTerminationOrTimeout(intervalMills)
+      if (!isStop && isExistsMarkFile(hdfs_file_path)){
+        logger.warn("2秒后关闭sparkStreaming程序......")
+        Thread.sleep(2000)
+        ssc.stop(true, true)
+      }
+    }
+  }
+
+  /**
+    * 负责启动守护的jetty服务
+    * @param port
+    * @param ssc
+    */
+  def daemonHttpServer(port: Int, ssc: StreamingContext): Unit = {
+    val server = new Server(port)
+    val context = new ContextHandler()
+    context.setContextPath("/close")
+    context.setHandler(new CloseStreamHandler(ssc))
+    server.setHandler(context)
+    server.start()
+  }
+
+  class CloseStreamHandler(ssc: StreamingContext) extends AbstractHandler{
+    override def handle(s: String, request: Request,
+                        httpServletRequest: HttpServletRequest,
+                        httpServletResponse: HttpServletResponse): Unit = {
+      logger.warn("开始关闭.....")
+      ssc.stop(true, true)
+      httpServletResponse.setContentType("text/html;charset=utf-8")
+      httpServletResponse.setStatus(HttpServletResponse.SC_OK)
+      val out = httpServletResponse.getWriter
+      println("close success")
+      request.setHandled(true)
+      logger.info("关闭成功 .......")
+    }
+  }
+
+}
---
+++ src/main/scala/utils/KafkaOffsetManager.scala
@@ -0,0 +1,61 @@
+package utils
+
+
+import kafka.common.TopicAndPartition
+import kafka.utils.ZkUtils
+import org.I0Itec.zkclient.ZkClient
+import org.apache.spark.rdd.RDD
+import org.apache.spark.streaming.kafka.HasOffsetRanges
+import org.apache.spark.streaming.kafka010.HasOffsetRanges
+import org.slf4j.LoggerFactory
+
+object KafkaOffsetManager {
+  val logger = LoggerFactory.getLogger(KafkaOffsetManager.getClass)
+  val ZK_ADDRESS = PropertiesUtil.getPropString("zk_address")
+
+  def readOffsets(zkClient: ZkClient, zkOffsetPath: String, topic: String): Option[Map[TopicAndPartition,Long]] ={
+    val(offsetsRangesStrop,_) = ZkUtils.readDataMaybeNull(zkClient, zkOffsetPath)
+    offsetsRangesStrop match {
+      case Some(offsetsRangesStr) =>
+        // 这个topic在zk连最新的分区数量
+        val lastest_partitions = ZkUtils.getPartitionsForTopics(zkClient, Seq(topic)).get(topic).get
+
+        var offsets = offsetsRangesStr.split(",") // 按逗号split成数组
+          .map(s => s.split(":"))  // 按冒号拆分每隔分区和偏移量
+          .map{case Array(partitionStr, offsetStr) =>(TopicAndPartition(topic, partitionStr.toInt) -> offsetStr.toLong)} // 加工成最终的格式
+          .toMap
+
+        // 说明有分区扩展了
+        if (offsets.size < lastest_partitions.size){
+          // 得到旧的所有分区序号
+          val old_partitions = offsets.keys.map(p => p.partition).toArray
+          // 通过差集得出来多的分区数组
+          val add_partitions = lastest_partitions.diff(old_partitions)
+          if (add_partitions.size>0){
+            logger.warn("发现kafka新增区: " + add_partitions.mkString(","))
+            add_partitions.foreach(partitionId =>{
+              offsets += (TopicAndPartition(topic, partitionId)->0)
+              logger.warn("新增分区id: " +partitionId + "添加完毕")
+            })
+          }
+        } else {
+          logger.warn("没有发现新增的kafka分区: " + lastest_partitions.mkString(","))
+        }
+        Some(offsets)  // 将Map返回
+      case None =>
+        None  // 如果是null，就返回None
+    }
+  }
+
+  def saveOffsets(zkClient: ZkClient, zkOffsetPath: String, rdd: RDD[_])={
+    // 转换RDD为Array[OffsetRange]
+    val offsetsRanges = rdd.asInstanceOf[HasOffsetRanges].offsetRanges
+    // 转换每个OffsetsRange为存储到zk时的字符串格式:  分区序号1:偏移量1,分区序号2:偏移量2,,......
+    val offsetsRangesStr = offsetsRanges.map(offsetRange => s"${offsetRange.partition}:${offsetRange.untilOffset}").mkString(",")
+    logger.debug("保存偏移量: " + offsetsRangesStr)
+    // 将最终的字符串结果保存到zk里面
+    ZkUtils.updatePersistentPath(zkClient, zkOffsetPath, offsetsRangesStr)
+  }
+
+
+}
